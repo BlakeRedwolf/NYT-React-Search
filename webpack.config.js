bf7d3e42003d960f36111172d375189735e4be0f
BlakeRedwolf <blakethedead@gmail.com>
2017-06-25 03:19:32 -0700

    Files initialized, left off @  webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+
+module.exports = {
+  context: __dirname,
+  devtool: debug ? "inline-sourcemap" : null,
+  entry: "./public/js/app.js",
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      }
+    ]
+  },
+  output: {
+    path: __dirname + "/public/js",
+    publicPath: "/public/js",
+    filename: "bundle.js"
+  },
+  plugins: debug ? [] : [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
+  ],
+};
